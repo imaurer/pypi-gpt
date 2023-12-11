50a3054b216c1a78b03080060da271e3bfd0a1ce
Ian Maurer <ian@genomoncology.com>
2023-12-11 13:32:06 -0500

    Documentation notes.

--- src/clepy/app.py
+++ src/clepy/app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="ClePy 12/11",
+    summary="Demonstration of FastAPI, Pydantic, and OpenAI GPTS.",
+    description="""
+Supports `markdown` including:
+- Bullets
+- And other things.
+    """.strip(),
+    version="0.1.0.beta12345",
+)
 
 
 @app.get("/")
