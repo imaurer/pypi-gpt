0b3639a8ad31ffb223aba0850683a20a61b5cb54
Ian Maurer <ian@genomoncology.com>
2023-12-11 20:11:14 -0500

    Added CORS

--- src/clepy/app.py
+++ src/clepy/app.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI, Path
+from fastapi.middleware.cors import CORSMiddleware
+
 from clepy import ProjectWithURLs, db
 
 app = FastAPI(title="ClePy 12/11")
+
+origins = [
+    "http://localhost",
+    "https://chat.openai.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/{name}/{version}/")
